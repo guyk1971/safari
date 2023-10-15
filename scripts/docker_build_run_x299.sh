f6dc452fef83af59baf4bb2a0b992f789a955857
guyk1971 <guy.koren@gmail.com>
2023-10-15 08:54:13 +0300

    adding docker script

---
+++ scripts/docker_build_run_x299.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# DIR is the directory where the script is saved (should be <project_root/scripts)
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd $DIR
+
+MY_UID=$(id -u)
+MY_GID=$(id -g)
+MY_UNAME=$(id -un)
+BASE_IMAGE=
+mkdir -p ${DIR}/.vscode-server
+LINK=$(realpath --relative-to="/home/${MY_UNAME}" "$DIR" -s)
+IMAGE=hyena_safari_${MY_UNAME}
+if [ -z "$(docker images -q ${IMAGE})" ]; then
+    # Create dev.dockerfile
+    FILE=dev.dockerfile
+
+    ### Pick Tensorflow / Torch based base image below
+    # echo "FROM nvcr.io/nvidia/tensorflow:23.01-tf2-py3" > $FILE
+    echo "FROM nvcr.io/nvidia/pytorch:23.09-py3" > $FILE
+
+    echo "  RUN apt-get update" >> $FILE
+    echo "  RUN apt-get -y install nano gdb time" >> $FILE
+    # echo "  RUN apt-get -y install nvidia-cuda-gdb" >> $FILE
+    echo "  RUN apt-get -y install sudo" >> $FILE
+    echo "  RUN (groupadd -g $MY_GID $MY_UNAME || true) && useradd --uid $MY_UID -g $MY_GID --no-log-init --create-home $MY_UNAME && (echo \"${MY_UNAME}:password\" | chpasswd) && (echo \"${MY_UNAME} ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers)" >> $FILE
+
+    # START: install any additional package required for your image here
+    echo "  WORKDIR /tmp/safari_build"
+    echo "  COPY ./requirements_cont.txt /tmp/safari_build/"
+    echo "  RUN pip install -r requirements_cont.txt && rm -rf /tmp/safari_build"
+    # END: install any additional package required for your image here
+
+
+    echo "  RUN mkdir -p $DIR" >> $FILE
+    echo "  RUN ln -s ${LINK}/.vscode-server /home/${MY_UNAME}/.vscode-server" >> $FILE
+    echo "  RUN echo \"fs.inotify.max_user_watches=524288\" >> /etc/sysctl.conf" >> $FILE
+    echo "  RUN sysctl -p" >> $FILE
+    echo "  USER $MY_UNAME" >> $FILE
+
+    
+    # create convenient bashrc. do we need the first line ?
+    echo "  COPY $DIR/../env/bashrc.txt /home/${MY_UNAME}/.bashrc" >> $FILE 
+    echo "  RUN source /home/${MY_UNAME}/.bashrc" >> $FILE
+    
+
+    echo "  WORKDIR $DIR/.." >> $FILE
+    echo "  CMD /bin/bash" >> $FILE
+
+    docker build -f dev.dockerfile -t ${IMAGE} .
+
+fi
+
+EXTRA_MOUNTS=""
+if [ -d "/home/${MY_UNAME}/work" ]; then
+    EXTRA_MOUNTS+=" --mount type=bind,source=/home/${MY_UNAME}/work,target=/home/${MY_UNAME}/work"
+fi
+if [ -d "/home/${MY_UNAME}/work_1" ]; then
+    EXTRA_MOUNTS+=" --mount type=bind,source=/home/${MY_UNAME}/work_1,target=/home/${MY_UNAME}/work_1"
+fi
+
+docker run \
+    --gpus \"device=all\" \
+    --privileged \
+    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm \
+    --mount type=bind,source=${DIR}/..,target=${DIR}/.. \
+    # --mount type=bind,source=/home/scratch.svc_compute_arch,target=/home/scratch.svc_compute_arch \
+    # --mount type=bind,source=/home/utils,target=/home/utils \
+    # --mount type=bind,source=/home/scratch.computelab,target=/home/scratch.computelab \
+    # ${EXTRA_MOUNTS} \
+    --name hyena_safari \
+    ${IMAGE}
+
+cd -
